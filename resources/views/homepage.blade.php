877b6675d39edbcf55f78eb8ebaa93f198a719cd
marcomammi02 <marcomammi2002@gmail.com>
2023-01-12 16:12:07 +0100

    first commit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,20 @@
+<?php
+$title = 'Hello World!';
+$text = 'This is a paragraph!';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+</head>
+<body>
+    <h1>{{ $title }}</h1>
+    <p>{{ $text }}</p>
+</body>
+</html>
