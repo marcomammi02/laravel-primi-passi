09aa4c1746d57a3ad7690ba1f49dea0a345d099b
marcomammi02 <marcomammi2002@gmail.com>
2023-01-13 09:45:02 +0100

    bonus

--- resources/views/link-di-prova.blade.php
+++ resources/views/link-di-prova.blade.php
@@ -1,21 +1,12 @@
-<?php
-$title = 'Hello World!';
-$text = 'This is a paragraph!';
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Homepage</title>
+    <title>Link di prova</title>
 </head>
 <body>
-    <h1>{{ $title }}</h1>
-    <p>{{ $text }}</p>
-    <a href="http://127.0.0.1:8000/link-di-prova">Link di prova</a>
+    <p>Pagina di prova</p>
 </body>
 </html>
